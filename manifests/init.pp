47485bc1296e2cdda97d7a1704d919e7bd87d82e
Eric B <ebouliane@gmail.com>
2015-11-30 19:55:32 -0500

    code cleanup

--- manifests/init.pp
+++ manifests/init.pp
@@ -15,14 +15,12 @@
 #   check_file     (String: default [/tmp/pp.scheduled-reboot])
 #     - The check file that is created with Start DateTime added to the end.
 #
-#
-#
 class scheduled_runonce (
   $class_enabled     = true,
-  $datetime_start    = "2012-06-10 12:53:00",
-  $datetime_end      = "2012-06-10 12:53:59",
-  $command_var       = "/sbin/shutdown -ra now",
-  $check_file_prefix = "/tmp/pp.scheduled-task-once",
+  $datetime_start    = '2012-06-10 12:53:00',
+  $datetime_end      = '2012-06-10 12:53:59',
+  $command_var       = '/sbin/shutdown -ra now',
+  $check_file_prefix = '/tmp/pp.scheduled-runonce',
 ) {
   # Verify if target host is Linux
   if $::kernel == 'Linux' {
@@ -30,26 +28,26 @@ class scheduled_runonce (
     $dt_current     = regsubst(generate('/bin/date', '+%s'),'\n','')
     $dt_start       = regsubst(generate('/bin/date', '-d',$datetime_start,'+%s'),'\n','')
     $dt_end         = regsubst(generate('/bin/date', '-d',$datetime_end,'+%s'),'\n','')
-    $check_file = "${check_file_prefix}.${dt_start}"
+    $check_file = '${check_file_prefix}.${dt_start}'
 
-	# Check if Class is disabled (Helps to disable some hosts)
+    # Check if Class is disabled (Helps to disable some hosts)
     if $class_enabled == true {
-	  # Check if Date/Time is within allowed period
+      # Check if Date/Time is within allowed period
       if $dt_start <= $dt_current and $dt_end >= $dt_current {
-	    # Prep Command that is to be run
-        $command_string = "touch ${check_file};${command_var}"
-        exec{"scheduled_reboot_command":
+        # Prep Command that is to be run
+        $command_string = 'touch ${check_file};${command_var}'
+        exec { "scheduled_reboot_command":
           command => $command_string,
           user    => root,
-          onlyif  => "/usr/bin/test ! -f ${check_file}",
+          onlyif  => '/usr/bin/test ! -f ${check_file}',
           path    => ['/usr/bin','/sbin'],
         }
-		notice('Reboot command will run. ${command_string}')
+        notice('Reboot command will run. ${command_string}')
       } else {
         notice('Not the right time. Current Date/Time: ${dt_current}. Start Date/Time: ${dt_start}. End Date/Time: ${dt_end}.')
       }
     } else {
-	  notice('Class disabled.')
-	}
+      notice('Class disabled.')
+    }
   }
 }
